49a66a17785a0f1477d142366ae3fe637f4a6d09
Rayhsz <1979381950@qq.com>
2022-03-28 23:05:51 +0800

    hsz ....

---
+++ src/hszTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author19793
+ * @Date2022/3/28 23:05
+ * @Version 1.0
+ */
+public class hszTest {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
