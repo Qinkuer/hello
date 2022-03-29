ce786db0adf859c7edd8e871280bb1d2c982c1a4
Rayhsz <1979381950@qq.com>
2022-03-29 10:21:00 +0800

    第三次提交

--- src/hszTest.java
+++ src/hszTest.java
@@ -7,5 +7,6 @@ public class hszTest {
     public static void main(String[] args) {
         System.out.println("Mar_29!");
         System.out.println("hello world!");
+        System.out.println("第三次修改");
     }
 }
